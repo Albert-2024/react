4a7cb88c1db8a0042a9342efbff8a7861c579b07
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-12 05:24:43 +0530

    use Reducer completed

--- src/useReducerMan/Reducer.js
+++ src/useReducerMan/Reducer.js
@@ -1,20 +1,27 @@
-import React,{useState} from 'react'
+import React,{useReducer} from 'react';
+
+const reducer = (state,action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return { count: state.count + 1, showText: state.showText };;
+    case 'toggleText':
+      return { count: state.count , showText: !state.showText };
+    default:
+      return state;
+    }
+  }
 
 const Reducer = () => {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
-  
-
+  const [state,dispatch] = useReducer(reducer,{count:0, showText:true});
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button onClick={() => {
-        setCount(count+1);
-        setShowText(!showText)
-      }}>
-        click me
-      </button>
-      {showText && <p>hello</p>}
+        dispatch({type:'INCREMENT'})
+        dispatch({type:'toggleText'})
+      }}
+      > Click me</button>
+      {state.showText && <h1>Text</h1>}
     </div>
   )
 }
