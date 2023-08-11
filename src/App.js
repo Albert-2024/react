a9538e0f8431d4bb584b1319b7e36c06fb050338
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-11 22:19:40 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import State from './useStateMan/State';
 import './App.css';
+import Reducer from './useReducerMan/Reducer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <State /> */}
+        <Reducer />
       </header>
     </div>
   );
