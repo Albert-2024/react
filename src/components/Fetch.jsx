4e90870337abe2ace1782618f21511f5bd8cb61a
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-13 11:28:22 +0530

    fetching data using axios

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Fetch() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((resp) => resp.json())
-      .then((apiData) => {
-        console.log(apiData);
-        setData(apiData.message);
-      });
-  }, []);
-  return (
-    <div>
-      <img width={500} src={data} />
-    </div>
-  );
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        axios.get("https://dog.ceo/api/breeds/image/random").then((res) => {
+            setData(res.data.message);
+            console.log(res.data.message);
+        })
+
+    }, []);
+    return (
+        <div>
+            <img width={500} src={data} />
+        </div>
+    );
 }
 
 export default Fetch;
