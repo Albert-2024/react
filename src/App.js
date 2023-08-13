a0483d038cf978c2aae8229396859e120ceb446e
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-13 11:20:46 +0530

    method 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import State from './useStateMan/State';
 import './App.css';
+import Fetch from './components/Fetch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <State /> */}
+        <Fetch />
       </header>
     </div>
   );
