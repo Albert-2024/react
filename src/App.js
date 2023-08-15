83688628b95e8f3d94e0c85d2013376b0b385fe1
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-15 10:01:26 +0530

    react stopped for axios and moved to node js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import State from './useStateMan/State';
 import './App.css';
+import Register from './Register';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <State /> */}
+        <Register />  
       </header>
     </div>
   );
